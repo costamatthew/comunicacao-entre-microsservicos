c9fca65475b89db1b319ddbb6d6dd0c1553ba877
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-10-03 22:12:23 -0300

    feat: create products models

---
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/model/Category.java
@@ -0,0 +1,25 @@
+package br.com.comunicacaoentremicrosservicos.productapi.modules.produto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CATEGORY")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+
+}
---
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/model/Supplier.java
@@ -0,0 +1,23 @@
+package br.com.comunicacaoentremicrosservicos.productapi.modules.produto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "SUPPLIER")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+}
