fb53d92518bc2e3ce24362bc65d2181a7e99841d
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-10-04 23:24:08 -0300

    Add CategoryRequest

--- product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/model/Category.java
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/model/Category.java
@@ -1,8 +1,10 @@
 package br.com.comunicacaoentremicrosservicos.productapi.modules.produto.model;
 
+import br.com.comunicacaoentremicrosservicos.productapi.modules.produto.dto.CategoryRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
@@ -21,5 +23,10 @@ public class Category {
     @Column(name = "DESCRIPTION", nullable = false)
     private String description;
 
+    public static Category of(CategoryRequest request) {
+        var category = new Category();
+        BeanUtils.copyProperties(request, category);
 
+        return category;
+    }
 }
