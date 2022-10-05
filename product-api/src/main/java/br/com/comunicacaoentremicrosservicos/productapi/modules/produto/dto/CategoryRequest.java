5f56ae4227c563d62fae5b8c6df7b4202db6e64f
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-10-04 23:29:49 -0300

    feat: create Category request and response DTO

---
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/dto/CategoryRequest.java
@@ -0,0 +1,10 @@
+package br.com.comunicacaoentremicrosservicos.productapi.modules.produto.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+
+    private String description;
+
+}
