1863ec9381c921167c881708555fc4c774dcfe6d
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-10-12 21:38:36 -0300

    feat: finish produto module

--- product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/controller/ProductController.java
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/product")
 public class ProductController {
 
-    @Autowired
+    @Autowired  
     private ProductService productService;
 
     @PostMapping
--- product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/dto/ProductRequest.java
+++ product-api/src/main/java/br/com/comunicacaoentremicrosservicos/productapi/modules/produto/dto/ProductRequest.java
@@ -1,11 +1,13 @@
 package br.com.comunicacaoentremicrosservicos.productapi.modules.produto.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
 
     private String name;
+    @JsonProperty("quantity_available")
     private Integer quantityAvailable;
     private Integer supplierId;
     private Integer categoryId;
