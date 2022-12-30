4c8a74d9f8719364614c4152a7b1d0f7fe5dfd24
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-12-30 15:52:50 -0300

    feat: create auth to check token

--- sales-api/src/config/secrets/secrets.js
+++ sales-api/src/config/secrets/secrets.js
@@ -1,3 +1,9 @@
 const env = process.env;
 
-export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://admin:123456@localhost:27017";
+export const MONGO_DB_URL = env.MONGO_DB_URL
+  ? env.MONGO_DB_URL
+  : 'mongodb://admin:123456@localhost:27017';
+
+export const API_SECRET = env.API_SECRET
+  ? env.API_SECRET
+  : 'ZGV2LWVuY29kZS1zZWNyZXQta2V5LTIwMjI=';
